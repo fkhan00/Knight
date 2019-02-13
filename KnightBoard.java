dee44264bc6ec035c4ca0d36b671259c4514b258
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-02-12 19:30:04 -0500

    finished solve wrapper class

--- KnightBoard.java
+++ KnightBoard.java
@@ -20,4 +20,19 @@ public class KnightBoard{
       }
     output += "\n";}
     return output;}
-  
+
+  public boolean solve(int startingRow, int startingCol){
+    for(int i = 0; i < board.length; i++){
+      for(int j = 0; j < board.length; j++){
+        if(board[i][j] != 0){
+          throw new IllegalStateException("please give an empty board");
+        }
+        if(startingRow < 0 || startingCol < 0){
+          throw new IllegalArgumentException("please enter non negative parameters");}}}
+    return solveH(startingRow, startingCol);}
+
+  public boolean solveH(int r, int c){
+    if(filled()){
+      return true;}
+    
+  }
