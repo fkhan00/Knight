0080992f628ba88d33c11b66447a238e79ffa96e
fkhan00 <43051637+fkhan00@users.noreply.github.com>
2019-02-12 20:41:02 -0500

    created solver method

--- KnightBoard.java
+++ KnightBoard.java
@@ -29,10 +29,28 @@ public class KnightBoard{
         }
         if(startingRow < 0 || startingCol < 0){
           throw new IllegalArgumentException("please enter non negative parameters");}}}
-    return solveH(startingRow, startingCol);}
+    return solveH( startingRow, startingCol, 0, 0);}
+  public boolean valid(int r, int c){
+    return r > 0 && c > 0 && r < board.length && c < board.length && board[r][c] == 0;}
 
-  public boolean solveH(int r, int c){
+  public boolean filled(){
+    for(int i = 0; i < board.length; i++){
+      for(int j = 0; j < board.length; j++){
+        if(board[i][j] == 0){
+          return false;}}}
+    return true;}
+
+
+  public boolean solveH( int r, int c, int incR, int incC){
     if(filled()){
       return true;}
-    
-  }
+    System.out.println(toString());
+    if(! valid (r, c)){
+      board[r - incR][c - incC] = 0;
+      return false;}
+    board[r][c] = 1;
+     return solveH( r + 1, c + 2, 1, 2) || solveH( r + 2, c + 1, 2, 1) || solveH( r - 1, c + 1, -1, 1) ||
+     solveH( r - 1, c + 2, -1, 2) ||
+     solveH( r - 2, c + 1, -2, 1) ||  solveH( r - 1, c - 2, -1, -2) || solveH( r - 2, c - 1, -2, -1) ||
+      solveH( r  + 1, c - 2, 1, -2);
+  }}
